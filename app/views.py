9808f23a0545a77b63123253ce61ea9f1309adc6
awanjila <abemuchikan@gmail.com>
2016-12-31 15:57:07 +0300

    adding the element of templates

--- app/views.py
+++ app/views.py
@@ -1,5 +1,27 @@
+from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World"
+	user = {'nickname': 'Wanjila'} #fake user
+	posts=[
+	{
+		'author':{'nickname':'Wanjila'},
+		'body':'Beautiful Day in portland'
+	},
+	{
+
+        'author': {'nickname': 'Susan'}, 
+        'body': 'The Avengers movie was so cool!'
+	},
+	{
+
+        'author': {'nickname': 'Babu'}, 
+        'body': 'You have to stop watching porn!'
+	}
+	]
+
+	return render_template('index.html', 
+							title='Home',
+							user=user,
+							posts=posts)
